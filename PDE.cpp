1b91a9d32f220f12be13816d1ed7ee8eea40f8fe
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-11-15 15:17:10 -0500

    update

---
+++ PDE.cpp
@@ -0,0 +1,16 @@
+//Se importan los paquetes a usar
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+using namespace std
+
+//Se declaran las condiciones iniciales
+double k = 1.62;
+double cp = 820;
+double p = 2.71;
+double v = k(cp*p);
+
+int main()
+{
+return 0;
+}
