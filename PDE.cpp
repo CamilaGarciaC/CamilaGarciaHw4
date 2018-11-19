6459bbdf1185d97ca715a6ff6b98ea4a13dd4fab
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-11-19 12:31:32 -0500

    condiciones iniciales

--- PDE.cpp
+++ PDE.cpp
@@ -1,16 +1,33 @@
+//Parte de este ejercicio se baso en la teoria del libro Landau.
+
 //Se importan los paquetes a usar
-#include <iostream>
+#include<iostream>
+#include<math.h>
 #include <stdlib.h>
-#include <math.h>
-using namespace std
+#include <stdio.h>
+#include <fstream>
+using namespace std;
 
-//Se declaran las condiciones iniciales
+//Se declaran las constantes y condiciones iniciales 
 double k = 1.62;
-double cp = 820;
-double p = 2.71;
-double v = k(cp*p);
+double cp = 820; 
+double p = 2.71*0.000001; //Se convierte a kg y metros.
+double n = 100;
+double dx = 50/n;
+double dy = dx;
+double Dx = 0; 
+double Dy = 0;
+double dt=0.00003;
+double nu=(k*dt)/(dx*dx*cp*p);
+int N = 100
+double past[N][N];
+double future[N][N];
+double vertical1=0;
+double horizontal1=0;
+double vertical2=0;
+double horizontal2=0;
+double average=0;
+int steps=50000;
+double average[steps];
+double c=(n*dx)/2;
 
-int main()
-{
-return 0;
-}
