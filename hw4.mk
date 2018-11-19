ef548e73c673db1c107bc3b5c9ff871fdef121ac
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-11-19 15:28:16 -0500

    casi listo mk

--- hw4.mk
+++ hw4.mk
@@ -1,2 +1,19 @@
-Resultados_hw4.pdf: Resultados_hw4.tex
+Resultados_hw4.pdf : ODE2.pdf Resultados_hw4.tex
+
 	pdflatex Resultados_hw4.tex
+
+
+ODE2.pdf : 10a70grados.txt Plots_hw4.py
+	python Plots_hw4.py
+
+grados45.txt : a.outODE
+	./a.outODE
+
+a.outODE : ODE.cpp
+	g++ -o a.outODE ODE.cpp
+
+promedioperiodica.txt : a.out
+	./a.out>>
+
+a.out : PDE.cpp
+	g++ PDE.cpp
